06975c1816496509a51804667538817ed312326c
Rylei Mindrum <rymindrum@gmail.com>
2024-03-07 15:32:14 -0700

    Created functions within person.py

--- src/person.py
+++ src/person.py
@@ -1,3 +1,26 @@
 class Person:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def color():
+        pass
+
+    def compete():
+        pass
+
+    def movement():
+        pass
+    
+    def cooperative():
+        pass
+
+    def defective():
+        pass
+
+    def neutral():
+        pass
+
+
+
+
 
