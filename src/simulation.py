3785db951f993a7d595092b08a7344322c2b182a
Nate Stott <nate.stott@pm.me>
2024-03-07 14:40:35 -0800

    delt with merge conflict

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,12 @@
 class Simulation:
-    pass
+    def __init__(self, number_of_people, verbose):
+        self.number_of_people = number_of_people
+        self.verbose = verbose
 
+    def statistics(self):
+        if self.verbose:
+            print(f"Number of people: {self.number_of_people}")
 
+    def evacuate(self):
+        if self.verbose:
+            print("Evacuating...")
