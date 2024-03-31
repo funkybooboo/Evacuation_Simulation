f4cee16ec35952f599f726df566de419d9cee606
Nate Stott <nate.stott@pm.me>
2024-03-31 09:30:30 -0600

    here is the new memory file

---
+++ src/memory.py
@@ -0,0 +1,39 @@
+class Memory:
+    def __init__(self):
+        self.doors = set()
+        self.exits = set()
+        self.stairs = set()
+        self.glasses = set()
+        self.obstacles = set()
+        self.walls = set()
+        self.empties = set()
+        self.fires = set()
+        self.people = set()
+        self.items = ['doors', 'exits', 'stairs', 'glasses', 'obstacles', 'walls', 'empties', 'fires', 'people']
+
+    def combine(self, other):
+        for item in self.items:
+            for location in getattr(other, item):
+                self.add(item, location)
+
+    def add(self, what, where):
+        if what is None or where is None:
+            return
+        if what not in self.items:
+            return
+        for item in self.items:
+            if item != what:
+                self.remove(item, where)
+        getattr(self, what).add(where)
+
+    def remove(self, what, where):
+        if what is None or where is None:
+            return
+        if what not in self.items:
+            return
+        if where in getattr(self, what):
+            getattr(self, what).remove(where)
+
+    def __str__(self):
+        return f"Doors: {self.doors}\nExits: {self.exits}\nStairs: {self.stairs}\nGlasses: {self.glasses}\nObstacles: {self.obstacles}\nWalls: {self.walls}\nEmpties: {self.empties}\nFires: {self.fires}\nPeople: {self.people}"
+
