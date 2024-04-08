faa1cb4923c747ed9999e91b24588e014ac73ba2
Nate Stott <nate.stott@pm.me>
2024-04-07 19:21:34 -0600

    add cl args for ai

--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ from simulation import Simulation
 def main(args):
     number_of_people = 100
     verbose = False
+    with_ai = False
     if len(args > 1) and args[0].isdigit():
         number_of_people = int(args[1])
     if len(args) > 2 and (args[1] == "-v" or args[1] == "--verbose"):
         verbose = True
-    with_ai = False
+    if len(args) > 3 and (args[2] == "-ai" or args[2] == "--ai"):
+        with_ai = True
     simulation = Simulation(number_of_people, verbose, with_ai)
     simulation.statistics()
     simulation.evacuate()
