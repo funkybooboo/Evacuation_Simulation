f7886bdd0a1ce2229077f8997889eccb2f0469a9
Nate Stott <nate.stott@pm.me>
2024-09-25 19:51:26 -0600

    move onto other projects

--- Server/Api/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import * as request from 'supertest';
-import app from '../src/index';
-
-describe('POST /run', () => {
-    it('should return 200 OK', () => {
-        return request(app).post('/run').expect(200);
-    });
-});
--- Server/Api/jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['Api/dist/'], // ignore the dist folder from tests
-};
-
